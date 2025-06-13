e143ef3696a2c3c9e2aae4194a8dd4b84198ddee
Marc Cavazos <marcavazos19@icloud.com>
2025-06-13 09:32:57 -0500

    moved link up to parent

--- src/app/_components/ImageCard.tsx
+++ src/app/_components/ImageCard.tsx
@@ -12,18 +12,16 @@ const ImageCard = ({
   width: number;
 }) => {
   return (
-    <div className="p-4 bg-white border border-company-purple rounded-lg hover:opacity-75 min-h-10 h-full">
-      <Link href={`/image/${data.id}`} aria-label="Image link" className="flex flex-col items-center ">
-        <h2 className="text-company-purple font-semibold tracking-wide p-2 text-center m-4">
-          {data?.title}
-        </h2>
-        <Image
-          src={data?.url}
-          height={height}
-          width={width}
-          alt={data?.title || `User image ${data?.id}`}
-        />
-      </Link>
+    <div className="flex flex-col items-center p-4 bg-white border border-company-purple rounded-lg  min-h-10 h-full">
+      <h2 className="text-company-purple font-semibold tracking-wide p-2 text-center m-4">
+        {data?.title}
+      </h2>
+      <Image
+        src={data?.url}
+        height={height}
+        width={width}
+        alt={data?.title || `User image ${data?.id}`}
+      />
     </div>
   );
 };
--- src/app/_components/ImageContainer.tsx
+++ src/app/_components/ImageContainer.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import ImageCard from "./ImageCard";
+import Link from "next/link";
 
 export type Photo = {
   url: string;
@@ -7,7 +8,7 @@ export type Photo = {
   description: string;
   title: string;
   user: number;
-}
+};
 
 export interface DataResponse {
   success: boolean;
@@ -17,12 +18,12 @@ export interface DataResponse {
 }
 
 const getData = async (): Promise<DataResponse | null> => {
-    const res = await fetch('http:/localhost:3000/api/images');
+  const res = await fetch("http:/localhost:3000/api/images");
 
-    const data = await res.json();
+  const data = await res.json();
 
-    return data;
-}
+  return data;
+};
 
 const ImageContainer = async () => {
   const mockImages = await getData();
@@ -33,17 +34,17 @@ const ImageContainer = async () => {
   return (
     <div className="flex flex-col gap-6">
       <span className="text-black font-semibold text-shadow-company-purple text-2xl">
-        {`${mockImages?.total_photos || ''} images`}
+        {`${mockImages?.total_photos || ""} images`}
       </span>
       <ul className="flex flex-row flex-wrap gap-8 items-center">
-
-        {/* iterate over the map and return an array of JSX elements containing the image card information */}
+        {/* iterate over the map and return an array of JSX elements containing the link to the card page and the image card information */}
         {(mockImages?.photos || []).map((img) => (
           <li key={img.id} className="flex-1/3">
-            <ImageCard data={img} height={200} width={200} />
+            <Link href={`/image/${img?.id}`} className="hover:opacity-75 cursor-pointer">
+              <ImageCard data={img} height={200} width={200} />
+            </Link>
           </li>
         ))}
-
       </ul>
     </div>
   );
